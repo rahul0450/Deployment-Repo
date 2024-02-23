64d803318bef1edb8ce7749c1f82028c25845e3f
rahul0450 <160465026+rahul0450@users.noreply.github.com>
2024-02-23 15:22:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.text("Sepal characteristics")
-    sepal_l = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
+    sepal_l = st.slider('Sepal length (cm)', 1.0, 8.0, 0.5)
     sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
 
 with col2:
-    st.text("Pepal characteristics")
-    petal_l = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
+    st.text("Petal characteristics")
+    petal_l = st.slider('Petal length (cm)', 1.0, 7.0, 0.5)
     petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
 
 st.text('')
